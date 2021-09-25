3a3f3edc14f12e2d3e08396c425ed23fe9303056
Bartek <bartlomiej.proczkowski@gmail.com>
2021-09-25 13:42:28 +0200

    gitignore updated

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a086c3b6a8ba8f1f1574fb93901b5d85'),
+      secret: env("ADMIN_JWT_SECRET", "a086c3b6a8ba8f1f1574fb93901b5d85"),
     },
   },
 });
